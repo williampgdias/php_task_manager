6179fcce1da042f92bee841773749d53071b7d75
William Dias <smdwilliam89@gmail.com>
2026-04-14 23:01:48 +0100

    feat(routes): add RESTful API routes for tasks.

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -6,3 +6,10 @@ use CodeIgniter\Router\RouteCollection;
  * @var RouteCollection $routes
  */
 $routes->get('/', 'Home::index');
+$routes->group('api', function ($routes) {
+    $routes->get('tasks', 'TaskController::index');
+    $routes->get('tasks/(:num)', 'TaskController::show/$1');
+    $routes->post('tasks', 'TaskController::create');
+    $routes->put('tasks/(:num)', 'TaskController::update/$1');
+    $routes->delete('tasks/(:num)', 'TaskController::delete/$1');
+});
